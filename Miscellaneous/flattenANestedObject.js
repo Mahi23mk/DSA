cb75991154bd9d0027fec671fc326c0ad150e401
Mahi23mk <34553013+Mahi23mk@users.noreply.github.com>
2026-08-10 02:31:04 -0700

    Update flattenANestedObject.js

--- Miscellaneous/flattenANestedObject.js
+++ Miscellaneous/flattenANestedObject.js
@@ -1,3 +1,21 @@
+const flattenANestedObject = (val)=>{
+    const stack = [{key: '', val}]
+    const final = {};
+    while(stack.length){
+      const {key: str, val: obj} = stack.pop();
+    
+      for(let [key,val] of Object.entries(obj)){  
+         if(typeof val === 'string'){
+          final[str ? `${str}.${key}`: key] = val;
+         }else{
+          stack.push({ key: str?`${str}.${key}` : key, val })
+         }
+      }
+    }
+    
+    return final;
+}
+
 const val = {
   user: {
     name: "Mahima",
@@ -13,22 +31,8 @@ const val = {
     }
   }
 }
+const res = flattenANestedObject(val);
 
-const stack = [{key: '', val}]
-const final = {};
-while(stack.length){
-  const {key: str, val: obj} = stack.pop();
-
-  for(let [key,val] of Object.entries(obj)){  
-     if(typeof val === 'string'){
-      final[str ? `${str}.${key}`: key] = val;
-     }else{
-      stack.push({ key: str?`${str}.${key}` : key, val })
-     }
-  }
-}
-
-console.log(final)
-
+console.log(res)
 
 //{user.name: 'Mahima', user.college.name: 'Gndu', user.address.city: 'Chandigarh', user.address.final.shape: 'triangle', user.address.final.circle: 'round'}
